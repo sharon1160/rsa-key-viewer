bd5a24f9e223e64ea0196f3d6d67b9df89491fe1
Sharon CR <schullunquia@unsa.edu.pe>
2022-11-08 19:40:12 -0500

    add index template

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+import src.load_keys
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Home</h1>"
+    return render_template("index.html")
 
 if __name__ == '__main__':
   app.run(debug=True)
